0a3ec6fcf52e2d9284ad0aabde98f0e73292f813
Nicolas Ollier <nicolas.f.ollier@gmail.com>
2022-04-01 19:24:07 +0200

    add behaviours and a pretty ugly pink style

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
 import { useState } from "react";
 import "./App.css";
+import data from "./data";
 
 function App() {
   const [count, setCount] = useState(0);
   const [text, setText] = useState([]);
 
+  const handleChange = (event) => {
+    setCount(event.target.value);
+  };
+
   const handleSubmit = (event) => {
     event.preventDefault();
-  }
+    setText(data.slice(0, count));
+  };
 
   return (
     <section className="section-center">
-      <h3>Tired of boring lorem ipsum?</h3>
+      <h3 className="main-title">Tired of boring lorem ipsum?</h3>
       <form className="lorem-form" onSubmit={handleSubmit}>
-        <label htmlFor="amount">Paragraphs:</label>
-        <input id="amount" name="amount" type="number" value={count} onChange={e => setCount(e.target.value)} />
-        <button type="submit">Generate</button>
+        <label htmlFor="amount">Number of paragraphs:</label>
+        <input
+          id="amount"
+          name="amount"
+          type="number"
+          min="0"
+          max="8"
+          value={count}
+          onChange={handleChange}
+        />
       </form>
-      <article>
-        
+      <article className="lorem-text">
+        {text.map((p) => {
+          return <p key={Math.random() * 1000}>{p}</p>;
+        })}
       </article>
     </section>
   );
